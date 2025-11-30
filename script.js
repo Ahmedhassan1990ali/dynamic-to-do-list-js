e924a1db029d18f4b577aae71024bbbd550663af
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-11-30 15:46:00 +0200

    Select DOM Elements

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
     
+
+    )
 });
