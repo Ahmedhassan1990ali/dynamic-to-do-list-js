05fa9b6cd3956ac711e6f03581af248c1e896d6d
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-11-30 15:49:46 +0200

    Create the addTask Function

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', function(){
     const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
+
+    function addTask(){
+        const taskText = taskInput.value.trim();
+        if (taskText === ""){
+            window.alert("enter a task");
+        }
+    }
     
 
     )
